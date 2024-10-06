b990d1842de8699a6923c74dea5040e1ebb6b908
Akhilesh <akhileshraj91@gmail.com>
2024-10-06 19:51:24 +0000

    code showing plots performance wrt problem size plotted

---
+++ tests/problem_size _analysis.py
@@ -0,0 +1,212 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+import tarfile
+from matplotlib import cm
+import warnings
+import yaml
+import csv
+import re
+
+plt.rcParams.update({'font.size': 12, "font.weight": "bold", 'axes.labelsize': 'x-large'})
+plt.tight_layout()  # Add this line to adjust the layout
+warnings.filterwarnings('ignore')
+
+current_working_directory = os.path.dirname(os.path.abspath(__file__))
+
+os.chdir(current_working_directory)
+
+exp_type = 'problem_size_identification' 
+experiment_dir = f'{current_working_directory}/experiment_data/{exp_type}/'
+
+root, apps, files = next(os.walk(experiment_dir))
+num_subs = len(apps)
+
+colormap = plt.cm.get_cmap('tab10', len(apps))
+
+def calculate_power_with_wraparound(current, previous, time_diff, wraparound_value=262143.328850):
+    diff = current - previous
+    if diff < 0:  # Wraparound detected
+        diff = (wraparound_value - previous) + current
+    return diff / time_diff
+
+def compute_power(pubEnergy):
+    power = {}
+    geopm_sensor0 = geopm_sensor1 = pd.DataFrame({'timestamp':[],'value':[]})
+    for i,row in pubEnergy.iterrows():
+        if i%2 == 0:
+            geopm_sensor0 = pd.concat([geopm_sensor0, pd.DataFrame([{'timestamp': row['time'], 'value': row['value']}])], ignore_index=True)
+        else:
+            geopm_sensor1 = pd.concat([geopm_sensor1, pd.DataFrame([{'timestamp': row['time'], 'value': row['value']}])], ignore_index=True)
+
+
+    power['geopm_power_0'] = pd.DataFrame({
+        'timestamp': geopm_sensor0['timestamp'][1:],  # Add timestamps
+        'power': [
+            calculate_power_with_wraparound(
+                geopm_sensor0['value'][i],
+                geopm_sensor0['value'][i-1],
+                geopm_sensor0['timestamp'][i] - geopm_sensor0['timestamp'][i-1]
+            ) for i in range(1, len(geopm_sensor0))
+        ]
+    })
+
+    # Apply the same logic to geopm_power_1
+    power['geopm_power_1'] = pd.DataFrame({
+        'timestamp': geopm_sensor1['timestamp'][1:],  # Add timestamps
+        'power': [
+            calculate_power_with_wraparound(
+                geopm_sensor1['value'][i],
+                geopm_sensor1['value'][i-1],
+                geopm_sensor1['timestamp'][i] - geopm_sensor1['timestamp'][i-1]
+            ) for i in range(1, len(geopm_sensor1))
+        ]
+    })
+
+    min_length = min(len(power['geopm_power_0']), len(power['geopm_power_1']))
+    geopm_power_0 = power['geopm_power_0'][:min_length]
+    geopm_power_1 = power['geopm_power_1'][:min_length]
+
+    average_power = pd.DataFrame({
+        'timestamp': geopm_power_0['timestamp'],  # Use the timestamp from geopm_power_0
+        'average_power': [(p0 + p1) / 2 for p0, p1 in zip(geopm_power_0['power'], geopm_power_1['power'])]
+    })
+    average_power['elapsed_time'] = average_power['timestamp'] - average_power['timestamp'].iloc[0]
+    power['average_power'] = average_power
+    return power
+
+def measure_progress(progress_data, energy_data):
+    Progress_DATA = {} 
+    progress_sensor = pd.DataFrame(progress_data)
+    first_sensor_point = min(energy_data['average_power']['timestamp'].iloc[0], progress_sensor['time'][0])
+    progress_sensor['elapsed_time'] = progress_sensor['time'] - first_sensor_point  # New column for elapsed time
+    # progress_sensor = progress_sensor.set_index('elapsed_time')
+    performance_elapsed_time = progress_sensor.elapsed_time
+    # Add performance_frequency as a new column in progress_sensor
+    frequency_values = [
+        progress_data['value'].iloc[t] / (performance_elapsed_time[t] - performance_elapsed_time[t-1]) for t in range(1, len(performance_elapsed_time))
+    ]
+    
+    # Ensure the frequency_values length matches the index length
+    frequency_values = [0] + frequency_values  # Prepend a 0 for the first index
+    progress_sensor['frequency'] = frequency_values
+    upsampled_timestamps= energy_data['average_power']['timestamp']
+    
+    # true_count = (progress_sensor['time'] <= upsampled_timestamps.iloc[0]).sum()
+
+    progress_frequency_median = pd.DataFrame({'median': np.nanmedian(progress_sensor['frequency'].where(progress_sensor['time'] <= upsampled_timestamps.iloc[0])), 'timestamp': upsampled_timestamps.iloc[0]}, index=[0])
+    for t in range(1, len(upsampled_timestamps)):
+        progress_frequency_median = pd.concat([progress_frequency_median, pd.DataFrame({'median': [np.nanmedian(progress_sensor['frequency'].where((progress_sensor['time'] >= upsampled_timestamps.iloc[t-1]) & (progress_sensor['time'] <= upsampled_timestamps.iloc[t])))],
+        'timestamp': [upsampled_timestamps.iloc[t]]})], ignore_index=True)
+    progress_frequency_median['elapsed_time'] = progress_frequency_median['timestamp'] - progress_frequency_median['timestamp'].iloc[0]
+    # Assign progress_frequency_median as a new column
+    Progress_DATA['progress_sensor'] = progress_sensor
+    Progress_DATA['progress_frequency_median'] = progress_frequency_median
+    return Progress_DATA
+
+# def collect_papi(PAPI_data):
+#     PAPI = {}
+#     for scope in PAPI_data['scope'].unique():
+#         # Extract the string between the 3rd and 4th dots
+#         scope_parts = scope.split('.')
+#         if len(scope_parts) > 4:  # Ensure there are enough parts
+#             extracted_scope = scope_parts[3]
+#             # Aggregate the data for the extracted scope using pd.concat
+#             PAPI[extracted_scope] = PAPI_data[PAPI_data['scope'] == scope]
+#             instantaneous_values = [0] + [PAPI[extracted_scope]['value'].iloc[k] - PAPI[extracted_scope]['value'].iloc[k-1] for k in range(1,len(PAPI[extracted_scope]))]
+#             # Normalize the instantaneous values between 0 and 10
+#             min_val = min(instantaneous_values)
+#             max_val = max(instantaneous_values)
+#             # PAPI[extracted_scope]['instantaneous_value'] = [(value - min_val) / (max_val - min_val) * 10 for value in instantaneous_values]
+#             PAPI[extracted_scope]['instantaneous_value'] = instantaneous_values
+#             PAPI[extracted_scope]['elapsed_time'] = PAPI[extracted_scope]['time'] - PAPI[extracted_scope]['time'].iloc[0]
+#             # PAPI[extracted_scope]['max_min'] = {}
+#     return PAPI
+
+def normalize(traces):
+    normalized_PAPI = {}
+    max_value = {'PAPI_L3_TCA': float('-inf'), 'PAPI_TOT_INS': float('-inf'), 'PAPI_TOT_CYC': float('-inf'), 'PAPI_RES_STL': float('-inf'), 'PAPI_L3_TCM': float('-inf')}
+    min_value = {'PAPI_L3_TCA': float('inf'), 'PAPI_TOT_INS': float('inf'), 'PAPI_TOT_CYC': float('inf'), 'PAPI_RES_STL': float('inf'), 'PAPI_L3_TCM': float('inf')}
+    for app in traces.keys():
+        for trace in traces[app]['data'].keys():
+            for scope in traces[app]['data'][trace]['papi'].keys():
+                max_value[scope] = max(max_value[scope],max(traces[app]['data'][trace]['papi'][scope]['instantaneous_value']))
+                min_value[scope] = min(min_value[scope],min(traces[app]['data'][trace]['papi'][scope]['instantaneous_value']))
+    for app in traces.keys():
+        for trace in traces[app]['data'].keys():
+            for scope in traces[app]['data'][trace]['papi'].keys():
+                traces[app]['data'][trace]['papi'][scope]['normalized_value'] = [(value - min_value[scope]) / (max_value[scope] - min_value[scope]) * 10 for value in traces[app]['data'][trace]['papi'][scope]['instantaneous_value']]
+    return traces
+
+traces = {}
+
+# Check if RESULTS directory exists, if not, create it
+results_dir = f'{current_working_directory}/experiment_data/RESULTS'
+if not os.path.exists(results_dir):
+    os.makedirs(results_dir)  # Create the directory if it doesn't exist
+
+for app_index, app in enumerate(apps):
+    cwd = root + '/' + app
+    if next(os.walk(cwd))[1] == []:
+        files = os.listdir(cwd)
+        for fname in files:
+            if fname.endswith("tar"):
+                # print(fname)
+                tar = tarfile.open(cwd + '/' + fname, "r")
+                tar.extractall(path=cwd + '/' + fname[:-4])
+                tar.close()
+    traces[app] = {'directories': next(os.walk(cwd))[1]}
+    traces[app]['data'] = {}
+    for trace in traces[app]['directories']:
+        traces[app]['data'][trace] = {}
+        pwd = f'{cwd}/{trace}'
+        pubProgress = pd.read_csv(f'{pwd}/progress.csv')
+        pubEnergy = pd.read_csv(f'{pwd}/energy.csv')
+        # pubPAPI = pd.read_csv(f'{pwd}/papi.csv')
+        parameters = yaml.safe_load(open(f'{pwd}/parameters.yaml'))  # Load parameters from YAML file
+
+        traces[app]['data'][trace]['power'] = compute_power(pubEnergy)
+        traces[app]['data'][trace]['progress'] = measure_progress(pubProgress,traces[app]['data'][trace]['power'])
+        # traces[app]['data'][trace]['papi'] = collect_papi(pubPAPI)
+        traces[app]['data'][trace]['PARAMS'] = parameters
+        
+# traces = normalize(traces)
+for app in traces.keys():
+    fig, axs = plt.subplots(2,1,figsize=(12,10))
+    fig_avg, axs_avg = plt.subplots(2,1,figsize=(12,10))
+    # fig_PAPI, axs_PAPI = plt.subplots(5,1,figsize=(12,16))
+    for trace in traces[app]['data'].keys():
+        color = colormap(app_index)  # Get a color from the colormap for each trace           
+        color = cm.tab10(len(traces[app]['data']))  # Get a color from the colormap for each trace
+        axs[0].scatter(traces[app]['data'][trace]['progress']['progress_frequency_median']['elapsed_time'], traces[app]['data'][trace]['progress']['progress_frequency_median']['median'], label=trace[-5:])  # Added label for legend
+        axs[1].scatter(traces[app]['data'][trace]['power']['average_power']['elapsed_time'], traces[app]['data'][trace]['power']['average_power']['average_power'], label=trace[-5:])  # Added label for legend
+        axs[0].grid(True)
+        axs[1].grid(True)
+        axs[1].set_xlabel("Elapsed Time (s)")  # Changed to set_xlabel
+        axs[0].set_ylabel("Progress")  # Changed to set_ylabel
+        axs[1].set_ylabel("Power (W)")  # Changed to set_ylabel
+        # axs[0].legend()  # Added legend for progress plot
+        # axs[1].legend()  # Added legend for power plot
+        fig.suptitle(app)
+        axs_avg[0].scatter(traces[app]['data'][trace]['PARAMS']['PROBLEM_SIZE'],np.mean(traces[app]['data'][trace]['progress']['progress_frequency_median']), color='r', s=50)
+        axs_avg[1].scatter(traces[app]['data'][trace]['PARAMS']['PROBLEM_SIZE'],traces[app]['data'][trace]['progress']['progress_frequency_median']['elapsed_time'].iloc[-1], color='g', s=50)
+        axs_avg[0].grid(True)
+        axs_avg[1].grid(True)
+        axs_avg[0].set_ylabel('Average Progress Reported')
+        axs_avg[1].set_ylabel('Execution Time Reported')
+        axs_avg[1].set_xlabel('Problem Size')
+        fig_avg.suptitle(f'{app} problem size vs performance')
+        # for i,scope in enumerate(traces[app]['data'][trace]['papi'].keys()):
+        #     axs_PAPI[i].scatter(traces[app]['data'][trace]['papi'][scope]['elapsed_time'], traces[app]['data'][trace]['papi'][scope]['normalized_value'], label=trace[-5:])
+        #     axs_PAPI[i].grid(True)
+        #     axs_PAPI[i].set_ylabel(f"{scope} value")
+        # axs_PAPI[i].set_xlabel("Elapsed Time (s)")
+        # fig_PAPI.suptitle(app)
+    
+    fig.savefig(f'{current_working_directory}/experiment_data/RESULTS/{app}.pdf')
+    fig_avg.savefig(f'{current_working_directory}/experiment_data/RESULTS/{app}_performance_vs_problemsize.pdf')
+    # fig_PAPI.savefig(f'{current_working_directory}/experiment_data/RESULTS/{app}_PAPI.pdf')
+    plt.show()
+# print(data)
---
+++ tests/problem_size_exp.py
@@ -0,0 +1,118 @@
+import time
+import numpy as np
+import sys
+import pandas as pd
+import nrm
+import csv
+import subprocess
+import os
+import tarfile
+import yaml
+
+#ACTIONS = [78.0, 83.0, 89.0, 95.0, 101.0, 107.0, 112.0, 118.0, 124.0, 130.0, 136.0, 141.0, 147.0, 153.0, 159.0, 165.0]
+ACTIONS = [165.0]
+PROBLEM_SIZE_ARRAY = [1000, 5000, 10000, 50000, 100000, 500000, 1000000, 5000000, 10000000, 25000000, 33554432]
+PCAP = ACTIONS[-1]
+# argument parser for the application
+
+i = 0
+APPLICATION = 'ones-stream-full'
+while i < len(sys.argv):
+    if sys.argv[i] == '--application':
+        APPLICATION = sys.argv[i+1]
+        i += 1
+    i +=1
+
+# define the problem size and iterations based on the applications
+
+if APPLICATION == 'ones-stream-full':
+    # PROBLEM_SIZE = 33554432
+    ITERATIONS = 10000
+
+# initialize the nrm clients
+
+client = nrm.Client()
+actuators = client.list_actuators()
+
+# start the experiment 
+experiment = 'problem_size_identification'
+EXP_DIR = f'./experiment_data/{experiment}/{APPLICATION}'
+if os.path.exists(EXP_DIR):
+    print("Directories exist")
+else:
+    os.makedirs(EXP_DIR)
+    print("Directory '%s' created") 
+
+# For post processing
+def compress_files(iteration):
+    tar_file = EXP_DIR+f'/compressed_iteration_{iteration}.tar'
+    with tarfile.open(tar_file, 'w:gz') as tarf:
+        for root, dirs, files in os.walk(EXP_DIR):
+            for file in files:
+                if file.endswith('.csv') or file.endswith('.yaml'):
+                    file_path = os.path.join(EXP_DIR, file)
+                    # rel_path = os.path.relpath(file_path, EXP_DIR)
+                    tarf.add(file_path, arcname=os.path.basename(file_path))
+                    # tarf.add(os.path.join(root, file), os.path.relpath(os.path.join(root, file), EXP_DIR))
+                    os.remove(file_path)
+
+    print(f'Compressed files into {tar_file}')
+
+# def organize_files(iteration):
+
+
+def experiment_for(PROBLEM_SIZE):
+    with open(f'{EXP_DIR}/measured_power.csv', mode='w', newline='') as power_file, open(f'{EXP_DIR}/progress.csv', mode='w', newline='') as progress_file, open(f'{EXP_DIR}/energy.csv', mode='w', newline='') as energy_file, open(f'{EXP_DIR}/parameters.yaml', 'w') as parameter_file:
+        power_writer = csv.writer(power_file)
+        progress_writer = csv.writer(progress_file)
+        energy_writer = csv.writer(energy_file)
+
+        # Write headers if files are empty
+        power_writer.writerow(['time', 'scope', 'value'])
+        progress_writer.writerow(['time', 'value'])
+        energy_writer.writerow(['time', 'scope', 'value'])
+        
+        # Create a dictionary to hold the parameters
+        parameters = {
+            'PCAP': PCAP,
+            'PROBLEM_SIZE': PROBLEM_SIZE
+        }
+        # Write the dictionary to the YAML file
+        yaml.dump(parameters, parameter_file)
+        
+
+        def cb(*args):
+            # print(args)
+            (sensor, time, scope, value) = args
+            scope = scope.get_uuid()
+            sensor = sensor.decode("UTF-8")
+            timestamp = time/1e9
+            if sensor == "nrm.benchmarks.progress":
+                progress_writer.writerow([timestamp, value])
+            elif sensor == "nrm.geopm.CPU_POWER":
+                # print(scope[-1])
+                power_writer.writerow([timestamp, scope[-1], value])
+            elif sensor == "nrm.geopm.CPU_ENERGY":
+                # print(scope[-1])
+                energy_writer.writerow([timestamp, scope[-1], value])
+
+        client.set_event_listener(cb)
+        client.start_event_listener("") 
+        process = subprocess.Popen(['ones-stream-full', f'{PROBLEM_SIZE}', '10000'])
+        client.actuate(actuators[0],PCAP)
+        while True:
+            time.sleep(1)
+            if process.poll() is not None:  
+                print("Process has completed.")
+                break
+    compress_files(PS)
+    print("----------------------------------")
+
+
+if __name__ == "__main__":
+    for PS in PROBLEM_SIZE_ARRAY:
+        experiment_for(PS)
+
+
+
+# compress the experiment details for post processing

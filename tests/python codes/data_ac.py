63b2818aa53071664aff1223cdf50d541e7ddcc4
Akhilesh <akhileshraj91@gmail.com>
2025-01-06 12:33:02 -0600

    data_ac.py changed

--- tests/python codes/data_ac.py
+++ tests/python codes/data_ac.py
@@ -44,15 +44,15 @@ with open('measured_power.csv', mode='w', newline='') as power_file, open('progr
     # print("--------", actuators)
     # Run the terminal command in the background
     # process = subprocess.Popen(['ones-npb-ep', '22', '1000'])
-    process = subprocess.Popen(['nrm-papiwrapper', '-i', '-e', 'PAPI_L3_TCA', '-e', 'PAPI_TOT_INS', '-e', 'PAPI_TOT_CYC', '-e', 'PAPI_RES_STL', '-e', 'PAPI_L3_TCM', '--', f'ones-solvers-cg', f'3000', 'poor', '0', f'1000'])
+    # process = subprocess.Popen(['nrm-papiwrapper', '-i', '-e', 'PAPI_L3_TCA', '-e', 'PAPI_TOT_INS', '-e', 'PAPI_TOT_CYC', '-e', 'PAPI_RES_STL', '-e', 'PAPI_L3_TCM', '--', f'ones-solvers-cg', f'3000', 'poor', '0', f'1000'])
 
-    #process = subprocess.Popen(['ones-stream-full', '33554432', '1000'])
+    process = subprocess.Popen(['ones-stream-full', '33554432', '1000'])
 
 
     # Keep the main script running
     while True:
         actuators = client.list_actuators()
-        client.actuate(actuators[0],130.0)
+        client.actuate(actuators[0],78.0)
         print(actuators)
         time.sleep(1)
         if process.poll() is not None:  # Process has completed

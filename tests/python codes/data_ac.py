2df2e429ac774580be034fab839f2045029ec8e6
AkhileshRaj <akhileshraj91@gmail.com>
2025-01-29 01:53:31 +0000

    something unwanted commited from the chameleon

--- tests/python codes/data_ac.py
+++ tests/python codes/data_ac.py
@@ -14,29 +14,31 @@ with open('measured_power.csv', mode='w', newline='') as power_file, open('progr
 
     # Write headers if files are empty
     if power_file.tell() == 0:
-        power_writer.writerow(['time', 'value'])
+        power_writer.writerow(['time', 'sensor', 'scope', 'value'])
 
     if progress_file.tell() == 0:
-        progress_writer.writerow(['time', 'value'])
+        progress_writer.writerow(['time', 'sensor', 'scope', 'value'])
     
     if energy_file.tell() == 0:
-        energy_writer.writerow(['time', 'value'])
+        energy_writer.writerow(['time', 'sensor', 'scope', 'value'])
 
 
     def cb(*args):
         # print(args)
         (sensor, time, scope, value) = args
+        # print(args)
+        scope = scope.get_uuid()
         sensor = sensor.decode("UTF-8")
         timestamp = time/1e9
         if sensor == "nrm.benchmarks.progress":
             # print(value)
-            progress_writer.writerow([timestamp, value])
+            progress_writer.writerow([timestamp, sensor, scope, value])
         elif sensor == "nrm.geopm.CPU_POWER":
             print(value)
-            power_writer.writerow([timestamp, value])
+            power_writer.writerow([timestamp, sensor, scope[-1], value])
         elif sensor == "nrm.geopm.CPU_ENERGY":
             # print(sensor)
-            energy_writer.writerow([timestamp, value])
+            energy_writer.writerow([timestamp, sensor, scope[-1], value])
 
     client.set_event_listener(cb)
     client.start_event_listener("") 
@@ -46,7 +48,7 @@ with open('measured_power.csv', mode='w', newline='') as power_file, open('progr
     # process = subprocess.Popen(['ones-npb-ep', '22', '1000'])
     # process = subprocess.Popen(['nrm-papiwrapper', '-i', '-e', 'PAPI_L3_TCA', '-e', 'PAPI_TOT_INS', '-e', 'PAPI_TOT_CYC', '-e', 'PAPI_RES_STL', '-e', 'PAPI_L3_TCM', '--', f'ones-solvers-cg', f'3000', 'poor', '0', f'1000'])
 
-    process = subprocess.Popen(['ones-stream-full', '33554432', '1000'])
+    process = subprocess.Popen(['phases-stream-full', '33554432', '10', '500'])
 
 
     # Keep the main script running
@@ -60,7 +62,8 @@ with open('measured_power.csv', mode='w', newline='') as power_file, open('progr
             client.actuate(actuators[0], 165.0)
             switch = True
         print(actuators)
-        time.sleep(1)
+        time.sleep(3)
         if process.poll() is not None:  # Process has completed
             print("Process has completed.")
             break
+

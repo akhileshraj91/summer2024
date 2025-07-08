f01f86da21771b58dcfd2669216672d4f6262c60
Akhilesh Raj <akhileshraj91@gmail.com>
2025-07-08 15:25:57 +0000

    resolved conflicts

--- data_ac.py
+++ data_ac.py
@@ -53,7 +53,7 @@ with open(f'{DATA_DIR}/measured_power.csv', mode='w', newline='') as power_file,
             client.actuate(actuators[0], 165.0)
             switch = True
         print(actuators)
-        time.sleep(10)
+        time.sleep(2)
         if process.poll() is not None:  
             print("Process has completed.")
             break

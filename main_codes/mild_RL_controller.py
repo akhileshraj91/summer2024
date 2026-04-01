9fb58d192f71a00bd9dda88d3d0bf1ea3a2b5c7b
Akhilesh <akhileshraj91@gmail.com>
2026-04-01 15:11:28 +0000

    one working model with 50x20 param size added to compare

--- main_codes/mild_RL_controller.py
+++ main_codes/mild_RL_controller.py
@@ -102,7 +102,7 @@ torch.set_num_interop_threads(1)
 policy_folder = '/home/cc/summer2024/main_codes/'
 policy_file = os.path.join(policy_folder, args.policy)
 
-model = FCNetwork(layers=[20, 20]).to(device)
+model = FCNetwork(layers=[50, 20]).to(device)
 # PyTorch < 2.0 does not support weights_only; try it, then fall back
 try:
     state = torch.load(policy_file, map_location=device, weights_only=True)
